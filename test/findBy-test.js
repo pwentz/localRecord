aabce9639d395e55b96124e2352b91f4b887406b
pwentz <patrick.w.wentz@gmail.com>
2016-11-17 14:49:04 -0700

    add validations for non-object params.

---
+++ test/findBy-test.js
@@ -0,0 +1,66 @@
+const assert = require('chai').assert
+const LocalRecord = require('./../lib/localRecord')
+
+describe('findBy', () => {
+  const localRecord = new LocalRecord()
+  beforeEach(() => localStorage.clear())
+
+  context('match found', () => {
+    context('query involves only one prop', () => {
+      it('returns the record', () => {
+        const record = { height: 'tall', eyes: 'brown' }
+
+        localRecord.create(record)()
+
+        const query = localRecord.findBy({ height: 'tall' })
+
+        assert.deepEqual(record, query)
+      })
+    })
+
+    context('query passes multiple props', () => {
+      it('returns the record that matches all props', () => {
+        const similarRecord = { height: 'tall', eyes: 'brown', sex: 'male' }
+        const matchingRecord = { height: 'tall', eyes: 'brown', sex: 'female' }
+
+        localRecord.create(matchingRecord)()
+        localRecord.create(similarRecord)()
+
+        const query = localRecord.findBy({ height: 'tall', sex: 'female' })
+
+        assert.deepEqual(matchingRecord, query)
+      })
+    })
+
+    context('multiple records match params', () => {
+
+      it('returns the first entry', () => {
+        const initialRecord = { hair: 'green', face: 'red' }
+        const matchingRecord = { hair: 'green', face: 'purple' }
+
+        localRecord.create(initialRecord)()
+        localRecord.create(matchingRecord)()
+
+        const query = localRecord.findBy({ hair: 'green' })
+
+        assert.deepEqual(query, initialRecord)
+      })
+    })
+  })
+
+  context('no match found', () => {
+    it('returns null', () => {
+      const nullRecord = localRecord.findBy({ eyes: 'blue' })
+
+      assert.isNull(nullRecord)
+    })
+  })
+
+  context('parameters passed are not an object', () => {
+    it('throws a TypeError', () => {
+      const attempt = localRecord.findBy.bind(localRecord, 'hair = blue')
+
+      assert.throws(attempt, TypeError, 'expected Object, but got String instead')
+    })
+  })
+})
---
+++ test/where-test.js
@@ -0,0 +1,55 @@
+const assert = require('chai').assert
+const LocalRecord = require('./../lib/localRecord')
+
+describe('where', () => {
+  const localRecord = new LocalRecord()
+  beforeEach(() => localStorage.clear())
+
+  context('match found', () => {
+    context('multiple instances have similar properties', () => {
+      it('returns an array of all matching records', () => {
+        const recordOne = { height: 'tall', eyes: 'green' }
+        const recordTwo = { height: 'tall', eyes: 'brown' }
+        const recordThree = { height: 'tall', eyes: 'blue' }
+
+        localRecord.create(recordOne)()
+        localRecord.create(recordTwo)()
+        localRecord.create(recordThree)()
+
+        const query = localRecord.where({ height: 'tall' })
+
+        assert.sameDeepMembers(query, [recordOne, recordTwo, recordThree])
+      })
+    })
+
+    context('one record has matching property', () => {
+      it('returns an array of matching record', () => {
+        const recordOne = { height: 'tall', eyes: 'red' }
+        const recordTwo = { height: 'short', eyes: 'indigo' }
+
+        localRecord.create(recordOne)()
+        localRecord.create(recordTwo)()
+
+        const query = localRecord.where({ height: 'short' })
+
+        assert.deepEqual(query, [recordTwo])
+      })
+    })
+  })
+
+  context('no match found', () => {
+    it('returns an empty array', () => {
+      const query = localRecord.where({ hair: 'brown' })
+
+      assert.deepEqual(query, [])
+    })
+  })
+
+  context('user passes any argument but object', () => {
+    it('throws a TypeError', () => {
+      const attempt = localRecord.where.bind(localRecord, 5)
+
+      assert.throws(attempt, TypeError, 'expected Object, but got Number instead')
+    })
+  })
+})
